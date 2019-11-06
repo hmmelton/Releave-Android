bba054fafe531e255ff4a2e4f15442d0b2c9dd06
Harrison Melton <hmmelton95@gmail.com>
2019-11-05 23:58:00 -0800

    holy fuck this is a pain in the ass

--- app/src/main/java/com/szr/android/DeepLinkUrls.kt
+++ app/src/main/java/com/szr/android/DeepLinkUrls.kt
@@ -1,6 +1,6 @@
 package com.szr.android
 
 object DeepLinkUrls {
-    const val EMAIL_RESET_DEEP_LINK = "srz-app.firebaseapp.com/reset"
-    const val EMAIL_VERIFICATION_DEEP_LINK = "srz-app.firebaseapp.com/verify"
+    const val EMAIL_RESET_DEEP_LINK = "https://szr-app.firebaseapp.com/reset?mode=resetPassword"
+    const val EMAIL_VERIFICATION_DEEP_LINK = "https://szr-app.firebaseapp.com/verify?mode=verifyEmail"
 }
